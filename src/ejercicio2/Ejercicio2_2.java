030d7baaa0d3cdba9f4f2652fe28b9129ab4fa13
German Rudyk <germanrudyk87@gmail.com>
2023-01-23 18:34:22 -0300

    Reagregado de menu

---
+++ src/ejercicio2/Ejercicio2_2.java
@@ -0,0 +1,35 @@
+/*
+Declarar una clase llamada Circunferencia que tenga como atributo privado el radio de
+tipo real. A continuación, se deben crear los siguientes métodos:
+a) Método constructor que inicialice el radio pasado como parámetro.
+b) Métodos get y set para el atributo radio de la clase Circunferencia.
+c) Método para crearCircunferencia(): que le pide el radio y lo guarda en el atributo del
+objeto.
+d) Método area(): para calcular el área de la circunferencia (���� = � ∗ ������).
+e) Método perimetro(): para calcular el perímetro (��������� = � ∗ � ∗ �����).
+ */
+package ejercicio2;
+
+import Entidad.Circunferencia;
+import Service.CircunferenciaServicio;
+
+
+
+/**
+ *
+ * @author German
+ */
+public class Ejercicio2_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Menu menu = new Menu();
+        
+        menu.menu();
+        
+    }
+    
+}
